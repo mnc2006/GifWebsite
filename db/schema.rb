e33604b72ef249dfd50c8c959248c9434aee3771
mnc2006 <mnc2006@hotmail.com>
2015-11-21 22:42:29 -0500

    deleted posts model, changed template.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151122031147) do
+
+  create_table "gifs", force: :cascade do |t|
+    t.string   "link"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
